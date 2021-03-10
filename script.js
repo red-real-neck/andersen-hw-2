d20209cbbdd2501cc7eda9c825fabe59bc12c165
Ruslan <faleot.karas@gmail.com>
2021-03-10 13:45:41 +0300

    first task finish

--- script.js
+++ script.js
@@ -1,54 +1,31 @@
 function makeObjectDeepCopy(obj) {
 
-    if (obj instanceof Object) {
-        let result;
-        obj instanceof Array ? result = [] : result = {};
-
-        for (key in obj) {
-            if (obj[key] instanceof Array) {
-                result[key] = obj[key].map((item, index, arr) => {
-                    if (!(arr[index] instanceof Array) && !(arr[index] instanceof Object)) {
-                        return item;
-                    } else {
-                        return makeObjectDeepCopy(item);
-                    }
-                });
-            } else if (typeof obj[key] === 'function') {
-                result[key] = obj[key];
-            } else if (obj[key] instanceof Object) {
-                result[key] = makeObjectDeepCopy(obj[key]);
-            } else {
-                result[key] = obj[key];
-            }
-
-        }
-
-        return result;
-    } else {
+    if (!(obj instanceof Object || obj instanceof Array)) {
         return obj;
     }
 
+    let result;
+    obj instanceof Array ? result = [] : result = {};
 
-}
-
-const kekObj = {
-    'instanceOfFunction': new Function('return "test"'),
-    'undefiendCheck': undefined,
-    0: null,
-    1: 'one',
-    2: 'two',
-    foo: n =>  {
-        return n;
-    },
-    nestedObjects: {
-        nastedArrays: [[0, 1, 2], [3, 4, {objInArray: 12334}], {nike: 'sneakers'}],
-        nestedFunction: {
-            prostoPrivet: function () {
-                return 'test';
-            }
+    for (key in obj) {
+        if (obj[key] instanceof Array) {
+            result[key] = obj[key].map((item, index, arr) => {
+                if (!(arr[index] instanceof Array) && !(arr[index] instanceof Object)) {
+                    return item;
+                } else {
+                    return makeObjectDeepCopy(item);
+                }
+            });
+        } else if (typeof obj[key] === 'function') {
+            result[key] = obj[key];
+        } else if (obj[key] instanceof Object) {
+            result[key] = makeObjectDeepCopy(obj[key]);
+        } else {
+            result[key] = obj[key];
         }
-    }
-}
 
-const newObj = makeObjectDeepCopy(kekObj);
-console.log(newObj);
+    }
+
+    return result;
+
+}
