370a72839aff5feeed81bebdf043e474c7c67a7a
Ruslan <faleot.karas@gmail.com>
2021-03-11 13:50:48 +0300

    done

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ function makeObjectDeepCopy(obj) {
 }
 
 function selectFromInterval(arr, firstValueOfInterval, secondValueOfInterval) {
-    if ( !(arr instanceof Array) || arr.some(item => !(typeof item === 'number')) || typeof firstValueOfInterval !== 'number' || typeof secondValueOfInterval !== 'number') {
+    if (!(arr instanceof Array) || arr.some(item => !(typeof item === 'number')) || typeof firstValueOfInterval !== 'number' || typeof secondValueOfInterval !== 'number') {
         throw new Error('error message');
     }
 
@@ -40,4 +40,28 @@ function selectFromInterval(arr, firstValueOfInterval, secondValueOfInterval) {
     result = arr.filter(item => item > startOfInterval && item <= endOfInterval);
 
     return result;
+}
+
+const myIterable = {
+    from: 5,
+    to: 10,
+    [Symbol.iterator]: function () {
+        if (myIterable.from > myIterable.to || (typeof myIterable.from !== 'number') || (typeof myIterable.to !== 'number')) {
+            throw new Error('Ошибка!');
+        }
+        let i = myIterable.from;
+        return {
+            next: function () {
+                if (i <= myIterable.to) {
+                    return { value: i++, done: false };
+                } else {
+                    return { done: true };
+                }
+            }
+        }
+    }
+}
+
+for (let item of myIterable) {
+    console.log(item);
 }
